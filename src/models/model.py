269049a423fc7ec6d41d472ef2ad4fa13ae6cf6e
Nov Larsley Salvador <nov.salvador@gmail.com>
2026-07-22 23:18:33 +0800

    first time defining a model

---
+++ src/models/model.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+
+class SimpleCNN(nn.Module):
+  def __init__(self):
+    super().__init__()
+
+    self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
+
+    self.relu = nn.ReLU()
+
+    self.pool = nn.MaxPool2d(kernel_size=2)
+
+    self.flatten = nn.Flatten()
+
+    self.fc = nn.Linear(16 * 16 * 16, 10 )
+
+  def forward(self, x):
+    x = self.conv1(x)
+    x = self.relu(x)
+    x = self.pool(x)
+    x = self.flatten(x)
+    x = self.fc(x)
+    return x 
+  
