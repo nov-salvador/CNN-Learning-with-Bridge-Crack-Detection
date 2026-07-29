10d990e85894a81237b2b0ce63d0c226f450da7d
Nov Larsley Salvador <nov.salvador@gmail.com>
2026-07-29 22:53:03 +0800

    changes the model

--- src/models/model.py
+++ src/models/model.py
@@ -1,3 +1,18 @@
 from torchvision.models import resnet18, ResNet18_Weights
+import torch.nn as nn
 
-MODEL = resnet18(weights=ResNet18_Weights.DEFAULT)
+from configs.config import NUM_CLASSES
+
+def get_model():
+  model = resnet18(weights=ResNet18_Weights.DEFAULT)
+  for params in model.parameters():
+    params.requires_grad = False
+
+  num_in_feature = model.fc.in_features
+
+  model.fc = nn.Linear(num_in_feature, NUM_CLASSES)
+    
+  for params in model.fc.parameters():
+    params.requires_grad = True
+  
+  return model
