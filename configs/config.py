a228dbeee706d99b27a28805e4b13378f5871963
Nov Larsley Salvador <nov.salvador@gmail.com>
2026-07-29 22:56:39 +0800

    creation of config file

--- configs/config.py
+++ configs/config.py
@@ -1,11 +1,18 @@
 import torch
 from pathlib import Path
 
-#Paths
-DATA_DIR = Path("data")
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+#Paths of data
+DATA_DIR = BASE_DIR / "data"
 TRAIN_DIR = DATA_DIR / "processed/train"
 VAL_DIR = DATA_DIR / "processed/val"
 TEST_DIR = DATA_DIR / "processed/test"
+SAMPLE_DIR_1 = DATA_DIR / "sample/sample_1.jpg"
+SAMPLE_DIR_2 = DATA_DIR / "sample/sample_2.jpg"
+SAMPLE_DIR_3 = DATA_DIR / "sample/sample_3.jpg"
+SAMPLE_DIR_4 = DATA_DIR / "sample/sample_4.jpg"
+SAMPLE_DIR_5 = DATA_DIR / "sample/sample_5.jpg"
 
 #Image Size
 IMAGE_SIZE = 224
@@ -14,6 +21,7 @@ IMAGE_SIZE = 224
 BATCH_SIZE = 32
 LEARNING_RATE = 0.001
 EPOCHS = 10
+RESUME_TRAINING = False
 
 #Model
 NUM_CLASSES = 2
@@ -22,9 +30,14 @@ NUM_CLASSES = 2
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 #Outputs
-OUTPUT_DIR = Path("outputs")
+OUTPUT_DIR = BASE_DIR / "outputs"
 PREDICTION_DIR = OUTPUT_DIR / "predictions"
 GRADCAM_DIR = OUTPUT_DIR / "gradcam"
 
 #Checkpoint
-CHECKPOINT_DIR = Path("checkpoints")
+CHECKPOINT_DIR = BASE_DIR / "checkpoints"
+
+#Logs
+LOG_DIR = BASE_DIR / "logs"
+CSV_LOG_FILE = LOG_DIR / "training_log.csv"
+TENSORBOARD_DIR = LOG_DIR / "tensorboard"
