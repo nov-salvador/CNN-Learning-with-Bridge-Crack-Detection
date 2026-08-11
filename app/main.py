320b1fbc7a19160985e9ed89c613f43cf790a64f
Nov Larsley Salvador <nov.salvador@gmail.com>
2026-08-11 13:16:01 +0800

    edited the cors for the api

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://bridge-crack-detection.netlify.app/"],
+    allow_origins=["https://bridge-crack-detection.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
